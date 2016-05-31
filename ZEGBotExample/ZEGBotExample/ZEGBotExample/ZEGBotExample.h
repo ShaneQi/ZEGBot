c7842ec711494411d9cea28f625cde6315bc6137
ShaneQi <qizengtai@gmail.com>
2016-05-30 22:32:14 -0500

    Create example project.

---
+++ ZEGBotExample/ZEGBotExample/ZEGBotExample/ZEGBotExample.h
@@ -0,0 +1,19 @@
+//
+//  ZEGBotExample.h
+//  ZEGBotExample
+//
+//  Created by Shane Qi on 5/30/16.
+//  Copyright © 2016 com.github.shaneqi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ZEGBotExample.
+FOUNDATION_EXPORT double ZEGBotExampleVersionNumber;
+
+//! Project version string for ZEGBotExample.
+FOUNDATION_EXPORT const unsigned char ZEGBotExampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZEGBotExample/PublicHeader.h>
+
+
