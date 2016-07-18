06438979e4cb2e74ca7c301961cf8eaca6ee6075
ShaneQi <qizengtai@gmail.com>
2016-07-18 14:21:59 -0500

    - Added LICENSE. - Fixed Package.swift. - Edited .gitignore. - Deleted .gitmodules.

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 import PackageDescription
 
 let package = Package(
-	name: "ZEGBot"
+	name: "ZEGBot",
 	dependencies: [
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", versions: Version(0,0,0)..<Version(10,0,0))
-			    ]
+	]
 )
