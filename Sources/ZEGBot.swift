190fa4ebb102f9b3c043b11455d5087081aaa494
ShaneQi <qizengtai@gmail.com>
2016-07-22 22:31:41 -0500

    Committed for debuting.

--- Sources/ZEGBot.swift
+++ Sources/ZEGBot.swift
@@ -6,46 +6,46 @@
 //
 //
 
-import PerfectCURL
-
-public struct ZEGBot {
-	
-	private var token: String
-	private var urlPrefix: String {
-		return "https://api.telegram.org/bot"+token+"/"
-	}
-	
-	init(token: String) {
-		
-		self.token = token
-		
-	}
-	
-	func runWith(handler: ZEGHandler?) {
-		var offset = 0
-		while true {
-			let curl = CURL()
-			curl.url = urlPrefix + "getupdates?timeout=60&offset=\(offset)"
-			
-			let responseBodyString = curl.performFully().2.reduce("", combine: { a, b in a + String(UnicodeScalar(b)) })
-			
-			let updates = ZEGDecoder.decodeUpdates(from: responseBodyString)
-			
-			for update in updates {
-				
-				handler?.handle(upadte)
-				
-			}
-			
-		}
-		
-	}
-	
-}
-
-public protocol ZEGHandler {
-
-	func handle(_ update: Update)
-
-}
-
+//import PerfectCURL
+//
+//public struct ZEGBot {
+//	
+//	private var token: String
+//	private var urlPrefix: String {
+//		return "https://api.telegram.org/bot"+token+"/"
+//	}
+//	
+//	init(token: String) {
+//		
+//		self.token = token
+//		
+//	}
+//	
+//	func runWith(handler: ZEGHandler?) {
+//		var offset = 0
+//		while true {
+//			let curl = CURL()
+//			curl.url = urlPrefix + "getupdates?timeout=60&offset=\(offset)"
+//			
+//			let responseBodyString = curl.performFully().2.reduce("", combine: { a, b in a + String(UnicodeScalar(b)) })
+//			
+//			let updates = ZEGDecoder.decodeUpdates(from: responseBodyString)
+//			
+//			for update in updates {
+//				
+//				handler?.handle(upadte)
+//				
+//			}
+//			
+//		}
+//		
+//	}
+//	
+//}
+//
+//public protocol ZEGHandler {
+//
+//	func handle(_ update: Update)
+//
+//}
+//
