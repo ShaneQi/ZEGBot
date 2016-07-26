474049a433d0c1b5fdda14124f4218284e6637eb
ShaneQi <qizengtai@gmail.com>
2016-07-26 04:53:29 +0000

    Added unit test class.

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import ZEGDecoderTestSuite   
+
+XCTMain([
+     testCase(ZEGDecoderTests.allTests),     
+])
---
+++ Tests/ZEGDecoder/ZEGDecoderTests.swift
@@ -0,0 +1,17 @@
+import XCTest
+@testable import ZEGBot
+
+class ZEGDecoderTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual("Hello, World!", "Hello, World!")
+    }
+
+
+    static var allTests : [(String, (ZEGDecoderTests) -> () throws -> Void)] {
+        return [
+            ("testExample", testExample),
+        ]
+    }
+}
