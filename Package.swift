fdac795745b0a769783cf83229314d94196b4395
Shane Qi <qizengtai@gmail.com>
2017-09-28 22:52:06 -0500

    Revised Package.swift for swift 4.

--- Package.swift
+++ Package.swift
@@ -1,8 +1,24 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
 	name: "ZEGBot",
+	products: [
+		.library(
+			name: "ZEGBot",
+			targets: ["ZEGBot"]),
+		],
 	dependencies: [
-		.Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 15)
-	]
+		.package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", from: Version(15, 0, 6))
+	],
+	targets: [
+		.target(
+			name: "ZEGBot",
+			dependencies: ["SwiftyJSON"],
+			path: "./Sources"),
+		.testTarget(
+			name: "ZEGBotTests",
+			dependencies: ["ZEGBot"]),
+		]
 )
