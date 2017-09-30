76bae02bd658f6e44d3815a49b5e9cb8e9f3b458
Shane Qi <qizengtai@gmail.com>
2017-09-30 00:30:32 -0500

    Got ready for release.

--- Example/main.swift
+++ Example/main.swift
@@ -8,18 +8,12 @@
 import ZEGBot
 import Foundation
 
-let bot = ZEGBot(token: "229521725:AAHM_LCcQ1vT3hV8_wP_YawriyBvfbpJN3A")
+let bot = ZEGBot(token: "TYPE YOUR TOKEN HERE")
 
 bot.run { updateResult, bot in
 	switch updateResult {
 	case .success(let update):
 		dump(update)
-		bot.send(chatAction: .uploadVideo, toChat: update.message!.chatId)
-		bot.send(message: "hello", to: update.message!)
-		bot.forward(message: update.message!, to: update.message!)
-		bot.send(photo: "AgADBQAD86cxGxcecFYNl5Ur6EaRZMsMzDIABAjm04jdYU5dgdUCAAEC", caption: "picture", to: update.message!)
-		bot.send(sticker: "CAADBQADlAEAAhETzQRnLs_zdvA7bAI", to: update.message!)
-		dump(bot.getFile(ofId: "AgADBQAD86cxGxcecFYNl5Ur6EaRZMsMzDIABAjm04jdYU5dgdUCAAEC"))
 	case .failure(let error):
 		dump(error)
 	}
