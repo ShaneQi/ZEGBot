afc4bc33e09c15b59c469598c41c03b3cb12b309
Shane Qi <qizengtai@gmail.com>
2018-07-07 22:25:51 -0500

    Added `value` & `error` convenient readonly property to access value and error.

--- Sources/Utilities.swift
+++ Sources/Utilities.swift
@@ -17,6 +17,22 @@ public enum Result<T>: Decodable where T: Decodable {
 		case ok, result, description
 	}
 
+	public var value: T? {
+		if case .success(let value) = self {
+			return value
+		} else {
+			return nil
+		}
+	}
+
+	public var error: Swift.Error? {
+		if case .failure(let error) = self {
+			return error
+		} else {
+			return nil
+		}
+	}
+
 	public init(from decoder: Decoder) {
 		do {
 			let container = try decoder.container(keyedBy: CodingKeys.self)
